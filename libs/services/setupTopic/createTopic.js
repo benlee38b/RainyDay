532b0f71b3da0427bc34152e4b26b952266f3bb2
Ben Lee <benjamintlee600@gmail.com>
2020-10-13 10:16:25 +0100

    added dynamodb table to serverless.yml

--- libs/services/setupTopic/createTopic.js
+++ libs/services/setupTopic/createTopic.js
@@ -1,3 +1,11 @@
 import AWS from '../../libs/aws-sdk';
+const sns = new AWS.SNS();
 
-exports.createTopic = (city, email) => {};
+exports.createTopic = async (city, email) => {
+  let params = {
+    Name: city,
+  };
+  try {
+    await sns.createTopic(params).promise();
+  } catch (error) {}
+};
