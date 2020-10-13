89de7c79cceab9befdf303a75d41b362bd81fa36
Ben Lee <benjamintlee600@gmail.com>
2020-10-13 16:48:26 +0100

    updated iam roles for lambda function using dynamodb

--- libs/services/setupTopic/createTopic.js
+++ libs/services/setupTopic/createTopic.js
@@ -1,11 +1,22 @@
 import AWS from '../../libs/aws-sdk';
 const sns = new AWS.SNS();
+const docClient = new AWS.DynamoDB.DocumentClient();
+const tableName = process.env.CITY_TABLE;
 
 exports.createTopic = async (city, email) => {
-  let params = {
+  let snsParams = {
     Name: city,
   };
+  let dynamodbParams = {
+    TableName,
+    Item: {
+      city,
+    },
+  };
   try {
+    let existingCity = await docClient.get(params).promise();
+    if (!existing) {
+    }
     await sns.createTopic(params).promise();
   } catch (error) {}
 };
